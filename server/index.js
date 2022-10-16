180dd1977f70907e49f6934f80138d72d1c49a76
Mateusz <matixplay202@gamil.com>
2022-10-16 14:00:42 +0200

    fix

--- server/index.js
+++ server/index.js
@@ -20,12 +20,21 @@ if(process.env.NODE_ENV === 'production') {
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+        const protocol = req.protocol;
+        const host = req.hostname;
+        const url = req.originalUrl;
+        const port = process.env.PORT || PORT;
+
+        const fullUrl = `${protocol}://${host}:${port}${url}`
+    
+        const responseString = `Full URL is: ${fullUrl}`;                       
+        console.log(responseString);
+        console.log(responseString);
+        console.log(responseString);
+        console.log(responseString);
+        console.log(responseString);
     });
-
-    const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-    console.log(fullUrl);
 }
-
 
 const port = process.env.PORT || 5000;
 
