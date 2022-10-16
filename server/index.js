969a314ae92c14a50dfcb723ec4601dd28eb82dd
Mateusz <matixplay202@gamil.com>
2022-10-16 13:52:41 +0200

    fix

--- server/index.js
+++ server/index.js
@@ -21,10 +21,9 @@ if(process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
     });
-    console.log(process.env.NODE_ENV);
-    console.log(process.env.NODE_ENV);
-    console.log(process.env.NODE_ENV);
-    console.log(process.env.NODE_ENV);
+
+    const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    console.log(fullUrl);
 }
 
 
